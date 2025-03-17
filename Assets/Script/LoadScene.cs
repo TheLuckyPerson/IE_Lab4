cf006a394178097823b6de4ea1e09e4846d08e04
TheLuckyPerson <tylerle122002@gmail.com>
2025-03-16 19:34:45 -0700

    bonus levels and scene reorder

--- Assets/Script/LoadScene.cs
+++ Assets/Script/LoadScene.cs
@@ -6,9 +6,10 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour
 {
     
-    public void PlayGame()
+    public void PlayGame(int selectedScene = -1)
     {
         int buildIdx = SceneManager.GetActiveScene().buildIndex + 1;
+        if (selectedScene != -1) buildIdx = selectedScene;
         SceneManager.LoadScene(buildIdx % SceneManager.sceneCountInBuildSettings);
     }
 
